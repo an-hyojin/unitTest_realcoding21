9549dc8dea3ba72aefb5e4b56bacd4a6d36bb5c3
yonniii <rkdtjdus753@gmail.com>
2019-05-03 17:36:48 +0900

    학생의 수가 잘 들어가는지 확인하는 test 추가

--- src/test/java/GradeCounterTest.java
+++ src/test/java/GradeCounterTest.java
@@ -4,5 +4,17 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 public class GradeCounterTest {
 
+    @Test
+    public void 학생의_수가_잘_count되는지_확인(){
+        Student student_01 = new Student();
+        Student student_02 = new Student();
+        student_01.setScore(80);
+        student_02.setScore(50);
+        GradeCounter gradeCounter = new GradeCounter(5);
+        gradeCounter.addStudent(student_01);
+        gradeCounter.addStudent(student_02);
+
+        assertThat(gradeCounter.numberOfStudents(),is(2));
+    }
 
 }
