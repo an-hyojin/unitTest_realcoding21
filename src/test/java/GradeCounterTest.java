840dab6df73f6a8075ade91218b8e177f7990fdc
yonniii <rkdtjdus753@gmail.com>
2019-05-03 17:38:10 +0900

    학생정보가 배열에 들어가는지 확인

--- src/test/java/GradeCounterTest.java
+++ src/test/java/GradeCounterTest.java
@@ -17,4 +17,17 @@ public class GradeCounterTest {
         assertThat(gradeCounter.numberOfStudents(),is(2));
     }
 
+    @Test
+    public void 학생정보가_배열에_들어가는지_확인(){
+        Student student_01 = new Student();
+        Student student_02 = new Student();
+        student_01.setScore(80);
+        student_02.setScore(50);
+        GradeCounter gradeCounter = new GradeCounter(5);
+        gradeCounter.addStudent(student_01);
+        gradeCounter.addStudent(student_02);
+
+        assertThat(gradeCounter.studentWithOrderOf(1),is(student_02));
+    }
+
 }
