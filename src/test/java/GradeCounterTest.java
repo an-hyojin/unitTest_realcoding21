0fb49af946df9d93f190b9d236d5dcf8eceb4143
dbgprud <dbgprud8@gmail.com>
2019-05-03 19:01:46 +0900

    class GPA 확인하는 테스트 추가

--- src/test/java/GradeCounterTest.java
+++ src/test/java/GradeCounterTest.java
@@ -36,4 +36,26 @@ public class GradeCounterTest {
         GradeCounter gradeCounter = new GradeCounter(4);
         assertEquals(gradeCounter.maxNumberOfStudents(),4);
     }
+
+    @Test
+    public void Class_GPA를_확인(){
+        Student student_01 = new Student();
+        Student student_02 = new Student();
+        Student student_03 = new Student();
+        Student student_04 = new Student();
+        student_01.setScore(80);
+        student_02.setScore(50);
+        student_03.setScore(90);
+        student_04.setScore(35);
+
+        GradeCounter gradeCounter = new GradeCounter(5);
+        gradeCounter.addStudent(student_01);
+        gradeCounter.addStudent(student_02);
+        gradeCounter.addStudent(student_03);
+        gradeCounter.addStudent(student_04);
+        double GPA = (double) (80+50+90+35)/(double)4;
+        assertThat(gradeCounter.classAverageGPA(),is(GPA));
+
+    }
+
 }
